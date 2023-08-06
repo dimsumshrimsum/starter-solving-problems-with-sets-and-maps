d29c5a2382c9f524a97ed6efd5863b1f242d5eb9
Zach Robles <zrobles1188@gmail.com>
2023-08-05 22:44:02 -0400

    initial commit

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,16 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+  const m1 = new Map();
+  const s1 = new Set();
+
+  for (let e of a) {
+    m1.set(e, e);
+  }
+  for (let e of b) {
+    if (m1.get(e)) {
+      s1.add(e);
+    }
+  }
+  return [...s1];
+}
 
 module.exports = intersection;
--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -2,6 +2,21 @@
  * return a Map of the unique letters and arrays of words that are made up of those letters
  * @param {array} words An array  of words
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+  const results = new Map();
+
+  for (let e of words) {
+    let word = e.toLowerCase();
+    let letters = [...new Set([...word])].sort().join("");
+    // let letters = new Set([...word]);
+    // letters = [...letters].sort().join("");
+    if (results.has(letters)) {
+      results.get(letters).push(word);
+    } else {
+      results.set(letters, [word]);
+    }
+  }
+  return results;
+}
 
 module.exports = sameLetters;
